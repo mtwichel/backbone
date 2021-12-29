3d5bd7333405678e57509627df8bca41eccfe1df
Marcus Twichel <marc.twichy@gmail.com>
2021-12-29 00:02:54 -0700

    fix: server using logging middleware (#9)

--- packages/backbone/lib/backbone.dart
+++ packages/backbone/lib/backbone.dart
@@ -2,6 +2,7 @@ library backbone;
 
 export 'src/authentication/authentication.dart';
 export 'src/bad_request_exception.dart';
+export 'src/current_gcp_project.dart';
 export 'src/endpoints/endpoints.dart';
 export 'src/logging.dart';
 export 'src/request_context.dart';
